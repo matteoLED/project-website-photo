ee59bb66d3a739478c0e88de4cb95796527e9184
matteoLED <matte971cr7cr8@gmail.com>
2023-05-11 23:04:10 +0200

    push

--- src/components/PhotoCategory.tsx
+++ src/components/PhotoCategory.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+// import React from 'react';
+// import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
-interface PhotoCategoryProps {
-  title: string;
-  photos: { id: number; src: string; alt: string }[];
-}
+// interface PhotoCategoryProps {
+//   title: string;
+//   photos: { id: number; src: string; alt: string }[];
+// }
 
-const PhotoCategory: React.FC<PhotoCategoryProps> = ({ title, photos }) => {
-  return (
-    <div className="my-5">
-      <h2 className="text-center mb-5">{title}</h2>
-      <Container>
-        <Row xs={1} sm={2} md={3} lg={4} className="g-4">
-          {photos.map((photo) => (
-            <Col key={photo.id}>
-              <Card>
-                <Card.Img variant="top" src={photo.src} alt={photo.alt} />
-                <Card.Body>
-                  <Button variant="primary">View</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </div>
-  );
-};
+// const PhotoCategory: React.FC<PhotoCategoryProps> = ({ title, photos }) => {
+//   return (
+//     <div className="my-5">
+//       <h2 className="text-center mb-5">{title}</h2>
+//       <Container>
+//         <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+//           {photos.map((photo) => (
+//             <Col key={photo.id}>
+//               <Card>
+//                 <Card.Img variant="top" src={photo.src} alt={photo.alt} />
+//                 <Card.Body>
+//                   <Button variant="primary">View</Button>
+//                 </Card.Body>
+//               </Card>
+//             </Col>
+//           ))}
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// };
 
-export default PhotoCategory;
+// export default PhotoCategory;
+export {};
